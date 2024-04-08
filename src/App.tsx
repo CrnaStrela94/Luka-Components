b5d418ca9eac73c0d3cb6cda670b4dfe590f9004
thelinus1997 <thelinus1997@gmail.com>
2024-04-08 10:10:47 +0200

    idk man xd

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
+import React from "react";
 import { DragAndDrop } from "./components/DragDrop/DragAndDrop";
 import { MagnifierImg } from "./components/MagnifierImg/MagnifierImg";
 import SlideButton from "./components/SlideButton/SlideButton";
-
+import MeatballMenu from "./components/meatballMenu/MeatballMenu";
 // import { Text } from "./components/Text";
 
-
 function App() {
-
   return (
     <>
       <DragAndDrop />
       <SlideButton />
       <MagnifierImg />
+      <MeatballMenu />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
